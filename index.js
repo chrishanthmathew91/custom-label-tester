108365332e4ac62583266976cd06e7917b4876b8
Chrishanth Rogers <chrishanthmathew91@gmail.com>
2022-01-06 12:57:20 +0530

    remove last log

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ console.log('Hello World in feature branch to merge');
 console.log('Should create a merge commit');
 console.log('Another line of log')
 console.log('This log is not to be added to staging');
-console.log('A new log to test!!');
