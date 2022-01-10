3c2bb351e230879b27ee4584972dacf40fb3026e
Chrishanth Rogers <chrishanthmathew91@gmail.com>
2022-01-10 18:24:06 +0530

    Add a new commit that should remove the label

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log('Should create a merge commit');
 console.log('Another line of log')
 console.log('This log is to be added to staging');
 console.log('A new line to test label');
+console.log('Hopefully the final log');
