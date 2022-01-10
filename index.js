3ca9126cb26e98047d07f674f15059e054cf2318
Chrishanth Rogers <chrishanthmathew91@gmail.com>
2022-01-10 18:49:44 +0530

    Final test - first commit

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ console.log('Another line of log')
 console.log('This log is to be added to staging');
 console.log('A new line to test label');
 console.log('Hopefully the final log');
+
+console.log('Final test - A new commit');
