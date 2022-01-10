726914b24e2cb435028d1d65ea79ec2e122bc209
Chrishanth Rogers <chrishanthmathew91@gmail.com>
2022-01-10 12:12:33 +0530

    Add a new commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log('Hello World in feature branch to merge');
 console.log('Should create a merge commit');
 console.log('Another line of log')
-console.log('This log is not to be added to staging');
+console.log('This log is to be added to staging');
